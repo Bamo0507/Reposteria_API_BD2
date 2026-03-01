c4e8728675eba8a4d351a002e979f08bd81b57a8
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2026-03-01 15:21:59 -0600

    feat: endpoints for orders in admin

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { connectDB } = require("./db/connection");
 // Declare all routes
 const authRoutes = require("./routes/auth/auth");
 const adminReviewsRoutes = require("./routes/admin/adminResenias")
+const adminOrdersRoutes = require("./routes/admin/adminPedidos")
 
 const app = express();
 app.use(cors());
@@ -14,7 +15,8 @@ app.use(express.json());
 
 // Declare routes for a module
 app.use("/auth", authRoutes);
-app.use("/admin", adminReviewsRoutes)
+app.use("/admin", adminReviewsRoutes);
+app.use("/admin", adminOrdersRoutes);
 
 const PORT = process.env.PORT || 3000;
 

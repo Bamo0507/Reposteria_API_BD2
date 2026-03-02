f39e48681e517d5a3ec439131ef72dcd0405a07c
adrianap0607 <adrianap2670@gmail.com>
2026-03-01 23:24:38 -0600

    Endpoints para resenias de cliente

---
+++ routes/client/clienteResenias.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../../controllers/client/clienteReseniasController");
+
+router.get("/", controller.getClientReviews);
+router.post("/", controller.createReview);
+router.put("/:id", controller.updateReview);
+router.delete("/bulk", controller.deleteReviews);
+router.delete("/:id", controller.deleteReview);
+
+module.exports = router;
